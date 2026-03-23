4c85135e05d3e0adc2b087cc0badb8a8a2cc8cca
Daniele Ferone <daniele.ferone@unina.it>
2026-03-23 16:41:50 +0100

    Update CMake configuration to use C++23 and refactor SymmetricMatrix access operators

--- include/dferone/containers/SymmetricMatrix.h
+++ include/dferone/containers/SymmetricMatrix.h
@@ -22,29 +22,20 @@ namespace dferone::containers {
             data_.resize(size);
             for (std::size_t i = 0; i < n_; ++i) {
                 for (std::size_t j = 0; j <= i; ++j) {
-                    this->operator()(i, j) = initializer;
+                    this->operator[](i, j) = initializer;
                 }
             }
         }
 
-        const T &operator()(std::size_t row, std::size_t col) const {
-            assert(row < n_);
-            assert(col < n_);
+        template<typename Self>
+        auto &operator[](this Self &&self, std::size_t row, std::size_t col) {
+            assert(row < self.n_);
+            assert(col < self.n_);
 
             if (row <= col) {
-                return data_[idx_upper(row, col, n_)];
+                return self.data_[self.idx_upper(row, col)];
             }
-            return data_[idx_upper(col, row, n_)];
-        }
-
-        T &operator()(std::size_t row, std::size_t col) {
-            assert(row < n_);
-            assert(col < n_);
-
-            if (row <= col) {
-                return data_[idx_upper(row, col)];
-            }
-            return data_[idx_upper(col, row)];
+            return self.data_[self.idx_upper(col, row)];
         }
 
     private:
